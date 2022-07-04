190e5d98535a43cbf02cdbb88419247eb880ac36
kenyontu <kenyontu@outlook.com>
2022-07-04 11:54:51 +0800

    add meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,8 @@ import styles from '~/styles/app.css'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Frontend Mentor | Bookmark landing page',
+  description: 'My solution for the Bookmark landing page on Frontend Mentor',
   viewport: 'width=device-width,initial-scale=1',
 })
 
