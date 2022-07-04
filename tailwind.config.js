140dad9ebc988b49aa0ae017142a3196dd14f73f
kenyontu <kenyontu@outlook.com>
2022-07-05 00:14:21 +0800

    add tailwind custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      rubik: ['Rubik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'primary-400': 'hsl(231, 69%, 60%)',
+        'secondary-400': 'hsl(0, 94%, 66%)',
+        'neutral-400': 'hsl(229, 8%, 60%)',
+        'neutral-800': 'hsl(229, 31%, 21%)',
+      },
+      backgroundImage: {
+        'dot-pattern': 'url(/images/bg-dots.svg)',
+      },
+    },
   },
   plugins: [],
 }
